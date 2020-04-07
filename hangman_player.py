c62626cd2b2d020011b9a4a5485612897c3c44f9
satyam756 <57908190+satyam756@users.noreply.github.com>
2020-04-07 14:01:04 +0530

    Add files via upload
    =====

---
+++ hangman_player.py
@@ -0,0 +1,21 @@
+import hangman_creater
+
+
+hangman = hangman_creater.Hangman('D:\\wordlist.txt')
+hangman.choose_the_word()
+hangman.fill_the_word_status()
+while True:
+    hangman.get_word_status()
+    hangman.guess_the_letter()
+
+    if(hangman.attempts_remaining == 0):
+        print("Out of attempts. The word was {}. Game over!".format(hangman.chosen_word))
+        break
+
+    elif (hangman.chosen_word == ''.join(hangman.word_status)):
+        print("Hurray! You won the game!")
+        break
+
+
+
+
